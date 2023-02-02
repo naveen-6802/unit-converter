c38ec77a6c560c4e69e79a75f9c6e272279654fd
Naveen Soni <86905136+naveen-6802@users.noreply.github.com>
2023-02-02 22:09:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,68 @@
+// javascript
+
+const inputEl = document.getElementById("input-el");
+const cnv = document.getElementById("cnv");
+const rst = document.getElementById("rst");
+const err = document.getElementById("error");
+const len = document.getElementById("len");
+const vol = document.getElementById("vol");
+const mass = document.getElementById("mass");
+
+function metToFeet(val) {
+    let mtF = val * 3.281;
+    return mtF.toFixed(3);
+}
+
+function feetToMet(val) {
+    let ftM = val / 3.281;
+    return ftM.toFixed(3);
+}
+
+function litToGal(val) {
+    let ltG = val * 0.264;
+    return ltG.toFixed(3);
+}
+
+function galToLit(val) {
+    let gtL = val / 0.264;
+    return gtL.toFixed(3);
+}
+
+function kilToPou(val) {
+    let ktP = val * 2.204;
+    return ktP.toFixed(3);
+}
+
+function pouToKil(val) {
+    let ptK = val / 2.204;
+    return ptK.toFixed(3);
+}
+
+rst.addEventListener("click", function() {
+        len.innerHTML = "";
+        vol.innerHTML = "";
+        mass.innerHTML = "";
+});
+
+cnv.addEventListener("click", function() {
+    
+    if(inputEl.value === "" || inputEl.value === 0) {
+        err.style.display = "inline";
+        setTimeout(function() {
+            err.style.display = "none";
+        }, 800)
+    }
+    else {
+        let mF = `${inputEl.value} meters = ${metToFeet(inputEl.value)} feet | ${inputEl.value} feets = ${feetToMet(inputEl.value)} meters`;
+    
+        let lG = `${inputEl.value} litres = ${litToGal(inputEl.value)} gallons | ${inputEl.value} gallons = ${galToLit(inputEl.value)} litres`;
+        
+        let kP = `${inputEl.value} kilograms = ${kilToPou(inputEl.value)} pounds | ${inputEl.value} pounds = ${pouToKil(inputEl.value)} kilograms`;
+    
+        inputEl.value = "";
+        
+        len.innerHTML = mF;
+        vol.innerHTML = lG;
+        mass.innerHTML = kP;
+    }
+});
